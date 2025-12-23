4bef99c39b1bfd1743f7e611362baa879d2238cc
DARKPINGGD <omerasarfino@gmail.com>
2025-12-23 21:44:23 +0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import LevelTable from "../components/LevelTable";
+
+const levels = [
+  { position: 1, name: "Acheron", creator: "Ryamu", verifier: "Zoink" },
+  { position: 2, name: "Slaughterhouse", creator: "Icedcave", verifier: "Zoink" },
+  { position: 76, name: "Artificial Ascent", creator: "ViPriN", verifier: "Riot" }
+];
+
+export default function Home() {
+  return (
+    <main
+      style={{
+        padding: "40px",
+        fontSize: "26px",
+        maxWidth: "1200px",
+        margin: "0 auto"
+      }}
+    >
+      <h1 style={{ fontSize: "48px", marginBottom: "30px" }}>
+        Extreme Demon List
+      </h1>
+      <LevelTable levels={levels} />
+    </main>
+  );
+}
